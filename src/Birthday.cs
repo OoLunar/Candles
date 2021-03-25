c28ae588108f610e7b325398ef4587a112f3fe5c
Lunar Starstrum <lunar@forsaken-borders.net>
2021-03-25 01:12:52 -0500

    Formatting fixes; More code comments; changed .gitignore to fit C# instead of Rust; Added .editorconfig

--- src/Birthday.cs
+++ src/Birthday.cs
@@ -3,19 +3,19 @@ using System.Text.Json.Serialization;
 
 namespace Candles
 {
-    public class Birthday
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; private set; }
+	public class Birthday
+	{
+		[JsonPropertyName("name")]
+		public string Name { get; private set; }
 
-        [JsonPropertyName("date")]
-        public DateTime Date { get; private set; }
+		[JsonPropertyName("date")]
+		public DateTime Date { get; private set; }
 
-        public bool IsToday() => Date == DateTime.Now;
-        public bool IsUpcoming()
-        {
-            DateTime now = DateTime.Now;
-            return Date < now.AddDays(14) && Date >= now;
-        }
-    }
+		public bool IsToday() => Date == DateTime.Now;
+		public bool IsUpcoming()
+		{
+			DateTime now = DateTime.Now;
+			return Date < now.AddDays(14) && Date >= now;
+		}
+	}
 }
--- src/ExtensionMethods.cs
+++ src/ExtensionMethods.cs
@@ -2,20 +2,20 @@ using Gtk;
 
 namespace Candles
 {
-    public static class ExtensionMethods
-    {
-        public static ListBoxRow CreateRow(string text)
-        {
-            Label label = new(text);
-            label.LineWrap = true;
+	public static class ExtensionMethods
+	{
+		public static ListBoxRow CreateRow(string text)
+		{
+			Label label = new(text);
+			label.LineWrap = true;
 
-            ListBoxRow row = new();
-            row.Selectable = true;
-            row.CanFocus = true;
-            row.Margin = 6;
-            row.Add(label);
+			ListBoxRow row = new();
+			row.Selectable = true;
+			row.CanFocus = true;
+			row.Margin = 6;
+			row.Add(label);
 
-            return row;
-        }
-    }
+			return row;
+		}
+	}
 }
